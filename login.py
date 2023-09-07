34dd77596c338492ce2a3fb3b6cb49bc1381c5ae
Lameck Mukoya <lmukoya96@outlook.com>
2023-09-07 16:40:25 +0300

    Some OOP & fixed issue with MySQL connection.

--- login.py
+++ login.py
@@ -1,62 +1,62 @@
 import mysql.connector
-from personal_info import get_personal_info
-from personal_info import store_personal_info
+from personal_info import User
 from software import welcome_to_software
 
-def existing_user():
-    # Establish a connection
-    cnx = mysql.connector.connect(
-        host="your_connection",
-        user="your_username",
-        password="your_password",
-        database="simple_softwaredb"
-    )
+class Login:
+    def existing_user():
+        # Establish a connection
+        cnx = mysql.connector.connect(
+            host="your_connection",
+            user="your_username",
+            password="your_password",
+            database="simple_softwaredb"
+        )
 
-    # Create a cursor
-    cursor = cnx.cursor()
+        # Create a cursor
+        cursor = cnx.cursor()
 
-    # Get login credentials
-    username = input("Username: ")
-    password = input("Password: ")
+        # Get login credentials
+        username = input("Username: ")
+        password = input("Password: ")
 
-    # Execute SQL query
-    query = "SELECT * FROM personal_info WHERE username = %s AND password = %s"
-    cursor.execute(query, (username, password))
+        # Execute SQL query
+        query = "SELECT * FROM personal_info WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
 
-    # Fetch the result
-    result = cursor.fetchone()
+        # Fetch the result
+        result = cursor.fetchone()
 
-    if result:
-        print("Welcome back, " + username)
+        if result:
+            print("Welcome, " + username)
 
-        #User is able to access software
-        welcome_to_software()
-    else:
-        print("Invalid username or password")
-        #User is sent back to the login page
-        existing_user()
+            #User is able to access software
+            welcome_to_software()
+        else:
+            print("Invalid username or password")
+            #User is sent back to the login page
+            Login.existing_user()
 
-    # Close the cursor and connection
-    cursor.close()
-    cnx.close()
+        # Close the cursor and connection
+        cursor.close()
+        cnx.close()
 
-def new_user():
-    # Get personal information from the user
-    info = get_personal_info()
+    def new_user():
+        # Get personal information from the user
+        info = User.get_personal_info()
 
-    # Store personal information in the database
-    store_personal_info(info)
+        # Store personal information in the database
+        User.store_personal_info(info)
 
-    #New user will now be promted to login
-    existing_user()
+        #New user will now be promted to login
+        Login.existing_user()
 
 def main():
     #Identify is user is new or existing
     user_info = input("New or existing user(new/existing): ")
     if user_info.lower() == "new":
-        new_user()
+        Login.new_user()
     elif user_info.lower() == "existing":
-        existing_user()
+        Login.existing_user()
     else:
         print("Invalid input...")
 
--- personal_info.py
+++ personal_info.py
@@ -1,100 +1,101 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-def get_personal_info():
-    # Prompt user for personal information
-    first_name = input("Enter your First Name: ")
-    last_name = input("Enter your Last Name: ")
-    date_of_birth = input("Enter your date of birth (YYYY-MM-DD): ")
-    phone_number = input("Enter your Phone Number: ")
-    country = input("Enter your Country: ")
-    street = input("Enter your Street: ")
-    city = input("Enter your City: ")
-    state = input("Enter your State: ")
-    email = input("Enter your Email: ")
-    username = input("Enter your Username: ")
-    password = input("Enter your Password: ")
-    
-    # Return personal information as a dictionary
-    personal_info = {
-        "First Name": first_name,
-        "Last Name": last_name,
-        "Date of Birth": date_of_birth,
-        "Phone Number": phone_number,
-        "Country": country,
-        "Street": street,
-        "City": city,
-        "State": state,
-        "Email": email,
-        "Username": username,
-        "Password": password
-    }
-    
-    return personal_info
+class User:
+    def get_personal_info():
+        # Prompt user for personal information
+        first_name = input("Enter your First Name: ")
+        last_name = input("Enter your Last Name: ")
+        date_of_birth = input("Enter your date of birth (DD-MM-YYYY): ")
+        phone_number = input("Enter your Phone Number: ")
+        country = input("Enter your Country: ")
+        street = input("Enter your Street: ")
+        city = input("Enter your City: ")
+        state = input("Enter your State: ")
+        email = input("Enter your Email: ")
+        username = input("Enter your Username: ")
+        password = input("Enter your Password: ")
+        
+        # Return personal information as a dictionary
+        personal_info = {
+            "First Name": first_name,
+            "Last Name": last_name,
+            "Date of Birth": date_of_birth,
+            "Phone Number": phone_number,
+            "Country": country,
+            "Street": street,
+            "City": city,
+            "State": state,
+            "Email": email,
+            "Username": username,
+            "Password": password
+        }
+        
+        return personal_info
 
 
-def store_personal_info(personal_info):
-    try:
-        # Connect to the MySQL server
-        cnx = mysql.connector.connect(
-            host="your_connection",
-            user="your_username",
-            password="your_password",
-            database="simple_softwaredb"
-        )
-
-    except mysql.connector.Error as error:
-        if error.errno == errorcode.ER_BAD_DB_ERROR:
-            # The database does not exist, so create it
+    def store_personal_info(personal_info):
+        try:
+            # Connect to the MySQL server
             cnx = mysql.connector.connect(
                 host="your_connection",
                 user="your_username",
                 password="your_password",
+                database="simple_softwaredb"
             )
+
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_BAD_DB_ERROR:
+                # The database does not exist, so create it
+                cnx = mysql.connector.connect(
+                    host="your_connection",
+                    user="your_username",
+                    password="your_password",
+                )
+                cursor = cnx.cursor()
+
+                # Create the database
+                cursor.execute("CREATE DATABASE simple_softwaredb")
+                cursor.execute("USE simple_softwaredb")
+                cursor.execute("CREATE TABLE personal_info (first_name TEXT(255), last_name TEXT(255), date_of_birth DATE, phone_number VARCHAR(15), country TEXT(255), street TEXT(255), city TEXT(255), state TEXT(255), email VARCHAR (50), username TEXT(255), password TEXT(255))")          
+                cnx.database = "simple_softwaredb"
+
+            else:
+                print("Error connecting to MySQL server:", error)
+                return
+
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
+
+        try:
+            # Connect to the MySQL database
+            cnx = mysql.connector.connect(
+                host="your_connection",
+                user="your_username",
+                password="your_password",
+                database="simple_softwaredb"
+            )
+
+            # Create a cursor object to interact with the database
             cursor = cnx.cursor()
 
-            # Create the database
-            cursor.execute("CREATE DATABASE simple_softwaredb")
-            cursor.execute("USE simple_softwaredb")
-            cursor.execute("CREATE TABLE personal_info (first_name TEXT(255), last_name TEXT(255), date_of_birth DATE, phone_number VARCHAR(15), country TEXT(255), street TEXT(255), city TEXT(255), state TEXT(255), email VARCHAR (50), username TEXT(255), password TEXT(255))")          
-            cnx.database = "simple_softwaredb"
+            # Define the SQL query
+            query = """
+            INSERT INTO personal_info (first_name, last_name, date_of_birth, phone_number, country, street, city, state, email, username, password)
+            VALUES (%(First Name)s, %(Last Name)s, %(Date of Birth)s, %(Phone Number)s, %(Country)s, %(Street)s, %(City)s, %(State)s, %(Email)s, %(Username)s, %(Password)s)
+            """
 
-        else:
-            print("Error connecting to MySQL server:", error)
-            return
+            # Execute the query
+            cursor.execute(query, personal_info)
 
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
+            # Commit the changes to the database
+            cnx.commit()
+            print("Personal information stored successfully!")
 
-    try:
-        # Connect to the MySQL database
-        cnx = mysql.connector.connect(
-            host="your_connection",
-            user="your_username",
-            password="your_password",
-            database="simple_softwaredb"
-        )
+        except mysql.connector.Error as error:
+            print("Error storing personal information:", error)
 
-        # Create a cursor object to interact with the database
-        cursor = cnx.cursor()
-
-        # Define the SQL query
-        query = """
-        INSERT INTO personal_info (first_name, last_name, date_of_birth, phone_number, country, street, city, state, email, username, password)
-        VALUES (%(First Name)s, %(Last Name)s, %(Date of Birth)s, %(Phone Number)s, %(Country)s, %(Street)s, %(City)s, %(State)s, %(Email)s, %(Username)s, %(Password)s)
-        """
-
-        # Execute the query
-        cursor.execute(query, personal_info)
-
-        # Commit the changes to the database
-        cnx.commit()
-        print("Personal information stored successfully!")
-
-    except mysql.connector.Error as error:
-        print("Error storing personal information:", error)
-
-    # Close the cursor and connection
-    cursor.close()
-    cnx.close()
+        # Close the cursor and connection
+        cursor.close()
+        cnx.close()
